6c02d9e6ce6866f69185dbaf933d5e4af4e35521
SCOTT <scott.kutler@gmail.com>
2020-03-19 18:37:31 -0400

    MVP CSS and HTML done v1

---
+++ script.js
@@ -0,0 +1,9 @@
+// Harry Potter Key: 
+//Scott.kutler@gmail.com
+// $2a$10$2PGvzZtaIOQopUvoB2BIeOYR/PZjTNS0QL7GmjXSICBXNPN5qBxyS
+//URL: https://www.potterapi.com/v1/
+
+
+
+console.log("UI Pattern Project Connected")
+
